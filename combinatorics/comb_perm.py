936f41e04e06ddd87db9def1a9d67cb31c45aa9f
nmusaelian-rally <nmusaelian@rallydev.com>
2018-12-22 11:25:34 -0700

    sync

---
+++ combinatorics/comb_perm.py
@@ -0,0 +1,6 @@
+#https://www.hackerrank.com/challenges/itertools-combinations/problem
+
+from itertools import combinations, permutations
+
+print (list(combinations('ABCDE',2)))
+print (list(permutations('ABCDE',2)))
